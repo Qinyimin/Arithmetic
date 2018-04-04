ca315a8e1e7df9d88ffccc15f13c1dafc28739ea
Alex_Qin <33251026+Qinyimin@users.noreply.github.com>
2018-04-04 09:12:37 +0800

    Update Full Permutation.cpp

--- Full Permutation.cpp
+++ Full Permutation.cpp
@@ -5,24 +5,19 @@
 using namespace std;
 
 void swap(int &a, int &b) {
-	/*
-	1、（交换list数组中的下标为a,b的元素）
-	*/
 	int temp;
 	temp = a;
 	a = b;
 	b = temp;
 }
 void perm(int list[], int low, int high) {
-	if (low==high) {   //2、补全(递归循环结束条件）
+	if (low==high) {   
 		for (int i = 0; i <= low; i++)
 			cout << list[i];
 		cout << endl;
 	}
-	else {
-		/*
-		3、（递归实现：每个元素与第一个元素交换；交换后,得到子序列,用函数perm得到子序列的全排列；最后,将元素交换回来,复原,然后交换另一个元素）
-		*/
+	else {	
+		//（递归实现：每个元素与第一个元素交换；交换后,得到子序列,用函数perm得到子序列的全排列；最后,将元素交换回来,复原,然后交换另一个元素）
 		for (int j = low; j <= high; j++) {
 			swap(list[low], list[j]);
 			perm(list, low + 1, high);
