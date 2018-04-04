00d6d2eed8b677a6a43212ceaf8cb055d812b300
Alex_Qin <33251026+Qinyimin@users.noreply.github.com>
2018-04-04 09:11:03 +0800

    Full Permutation

---
+++ Full Permutation.cpp
@@ -0,0 +1,39 @@
+﻿//全排列问题
+#include"stdafx.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void swap(int &a, int &b) {
+	/*
+	1、（交换list数组中的下标为a,b的元素）
+	*/
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+void perm(int list[], int low, int high) {
+	if (low==high) {   //2、补全(递归循环结束条件）
+		for (int i = 0; i <= low; i++)
+			cout << list[i];
+		cout << endl;
+	}
+	else {
+		/*
+		3、（递归实现：每个元素与第一个元素交换；交换后,得到子序列,用函数perm得到子序列的全排列；最后,将元素交换回来,复原,然后交换另一个元素）
+		*/
+		for (int j = low; j <= high; j++) {
+			swap(list[low], list[j]);
+			perm(list, low + 1, high);
+			swap(list[low], list[j]);
+		}
+	}
+}
+int main()
+{
+	int list[] = { 1,2,3 };
+	perm(list, 0, 2);
+	system("PAUSE");
+	return 0;
+}
